1f1b09a09ec58284e5c28da701be496e2d1faa03
platon-reshetnikov <82944686+platon-reshetnikov@users.noreply.github.com>
2025-10-08 01:44:53 +0300

    Fix: align AutoMapper versions to 12.0.1; make entity string props nullable; add gateway/users changes

--- src/CategoryService/obj/Debug/net8.0/CategoryService.AssemblyInfo.cs
+++ src/CategoryService/obj/Debug/net8.0/CategoryService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520e48f44c3d6f79aa34a64c4d6ed4c98856c42f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a300da0915b2b7627eb12efd7a534beacd6cc51")]
 [assembly: System.Reflection.AssemblyProductAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CategoryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
