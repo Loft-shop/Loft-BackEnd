8bdc9c6b73b893a8bf0566df14727ed958ea4a57
platon-reshetnikov <82944686+platon-reshetnikov@users.noreply.github.com>
2025-10-28 12:22:28 +0200

    Up

--- src/CartService/obj/Debug/net8.0/CartService.AssemblyInfo.cs
+++ src/CartService/obj/Debug/net8.0/CartService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CartService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69926d1d74dadb996bcef974907e67c84137c909")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c03a3322811e3182eccb706ec2ba777c398da51")]
 [assembly: System.Reflection.AssemblyProductAttribute("CartService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CartService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
