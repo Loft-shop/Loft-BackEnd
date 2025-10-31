041d3a0f7f0582be5d3ab3b2e1064286abb2fffe
Klimplaneta <UpmailG@gmail.com>
2025-10-31 19:56:53 +0200

    fix

--- src/Common/Loft.Common/DTOs/CartDTO.cs
+++
@@ -1,3 +0,0 @@
-namespace Loft.Common.DTOs;
-
-public record CartDTO(long Id, long CustomerId, DateTime CreatedAt, ICollection<CartItemDTO> CartItems);
--- src/Common/Loft.Common/DTOs/ShippingAddressDTO.cs
+++
@@ -1,3 +0,0 @@
-namespace Loft.Common.DTOs;
-
-public record ShippingAddressDTO(long Id,long CustomerId,string Address,string City,string PostalCode,string Country);
